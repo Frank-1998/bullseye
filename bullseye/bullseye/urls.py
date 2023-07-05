8bac90e170304d1d362e049e1e1078a98d8ac28d
unknown <ziang6@ualberta.ca>
2023-07-04 23:56:56 -0600

    fix icon

--- bullseye/bullseye/urls.py
+++ bullseye/bullseye/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bullseye.views import index
+from . views import index
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
 ]
